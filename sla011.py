902b3a027037ce220af425317b2101ab11929336
gustavosilva06784-sketch <gustavosilva06784@gmail.com>
2026-09-14 21:06:20 -0300

    Etudos 002

---
+++ sla011.py
@@ -0,0 +1,15 @@
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade :"))
+senha = int(input("digite a sua senha: "))
+
+nomes = ["joao", "ana", "alicia", "anitha", "isabelly"]
+
+senha == 1234
+if senha == 1234:
+    print("acesso permitido")
+for nomes in nomes:
+    print("bem vindo, aqui esta a sua lista de nomes:", nomes)
+    
+else:
+    print("acesso negado")
+    exit()
